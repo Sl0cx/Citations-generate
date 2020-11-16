5d7c707e8fa1ad589a614c2ed9b7179dd4b95af4
Sl0cx <74133461+Sl0cx@users.noreply.github.com>
2020-11-16 22:57:59 +0100

    Update citations.js

--- src/citations.js
+++ src/citations.js
@@ -6,5 +6,37 @@ module.exports = {
     citation2: {
     auteur: 'Master Yoda',
     citation: 'No. Try not. Do... or do not. There is no try.'
+   },
+    citation3: {
+    auteur: 'Master Yoda',
+    citation: 'War does not make one great.'
+   },
+    citation4: {
+    auteur: 'Master Yoda',
+    citation: 'Fear is the path to the dark side. Fear leads to anger. Anger leads to hate. Hate leads to suffering.'
+   },
+       citation5: {
+    auteur: 'Princess Leïa',
+    citation: 'Help me ObiWan Kenobi, you are my only hope'
+   },
+       citation6: {
+    auteur: 'Master Obi-Wan Kenobi',
+    citation: 'The force is an energy field created by all living things, it surrounds us, it penetrates us, it binds the galaxy together'
+   },
+       citation7: {
+    auteur: 'Vador',
+    citation: 'Just for once, let me look on you with my own eyes.'
+   },
+       citation8: {
+    auteur: 'Luke Skywalker',
+    citation: 'Your thoughts betray you father. I feel the good in you. The conflict.'
+   },
+       citation9: {
+    auteur: 'Master Yoda',
+    citation: 'When 900 years old you reach. Look as good, you will not. Hmmm ?'
+   },
+       citation10: {
+    auteur: 'Master Plo Koon',
+    citation: 'When you ask for trouble, you should not be surprised when it finds you.'
    }
 };
