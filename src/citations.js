7e3dcdf0cff0f6aafba0cfac91d32ba0efa4a0e0
Sl0cx <74133461+Sl0cx@users.noreply.github.com>
2020-11-16 22:45:49 +0100

    Update citations.js

--- src/citations.js
+++ src/citations.js
@@ -1,5 +1,10 @@
 module.exports = {
-citation1: {
+    citation1: {
     auteur: 'Master Yoda',
-    citation: 'War does not make one great.',
+    citation: 'War does not make one great.'
+   },
+    citation2: {
+    auteur: 'Master Yoda',
+    citation: 'No. Try not. Do... or do not. There is no try.'
    }
+};
